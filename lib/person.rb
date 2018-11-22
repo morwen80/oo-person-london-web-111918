26dafbe1bc4d89ff011bd886296b508fcf28da6d
Valeria Ragonese <ribesnero80@gmail.com>
2018-11-22 16:05:37 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,11 +2,21 @@ class Person
  
  attr_reader :name 
  
- attr_accessor :bank_account
+ attr_accessor :bank_account :happiness
  
  def initialize
    @name = name
    @bank_account = $25
+   @happiness = 8
+   @hygiene = 8
+ end
+ 
+ def clean?
+   @hygiene > 7
+ end
+ 
+ def happy?
+   @happiness > 7
  end
  
 end
